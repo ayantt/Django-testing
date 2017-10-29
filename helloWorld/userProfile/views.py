178f769d7e43be03b1033feed0f9ba6cdcc0c8af
ayantt <ayantt@live.com>
2017-10-29 17:13:29 +0600

    Add files via upload

---
+++ helloWorld/userProfile/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from .models import User, Location
+
+
+def user(request):
+    all_user = User.objects.all()
+    all_location = Location.objects.all()
+    context = {
+        'all_user': all_user,
+        'all_location': all_location,
+    }
+    return render(request, 'userProfile/user.html', context)
+
+
+def details(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'userProfile/info.html', {'user': user})
+
+
+def addUser(request):
+    context = {}
+    return render(request, 'userProfile/addUser.html', context)
+
+
+def hello(request):
+    context = {}
+    return render(request, 'userProfile/home.html', context)
